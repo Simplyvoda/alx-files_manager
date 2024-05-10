e4781893d6c7cb07c711798e5c4604735e1438ec
Simplyvoda <efem.vodina@gmail.com>
2024-05-10 23:48:03 +0100

    changing app startup because of middleware issue

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // load all routes from routes/index.js
-app.use(routes);
+const router = express.Router();
+router.use('/', routes);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
