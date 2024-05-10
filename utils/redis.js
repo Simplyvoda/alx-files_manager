6995d46d5f430094005edb482704ad79369c7ea8
Simplyvoda <efem.vodina@gmail.com>
2024-05-10 12:14:05 -0700

    task 0

--- utils/redis.js
+++ utils/redis.js
@@ -4,7 +4,36 @@ import { createClient } from 'redis';
 class RedisClient {
 	constructor() {
 		this.client = createClient()
+		this.client.on('error', err => console.log('Redis Client Error', err)).
+			connect();
 	}
 
+	function isAlive() {
+		client.on('error', (err) => {
+  return false
+});
+
+client.on('connect', () => {
+  return true
+});
 
 }
+
+async function get(key){
+	const value = await this.client.get(key)
+	return value;
+}
+
+async function set(key, value, dur){
+	await this.client.setex(key, dur, value)
+}
+
+async function del(key){
+	await this.client.del(key)
+}
+
+}
+
+const redisClient = RedisClient()
+
+export redisClient;
