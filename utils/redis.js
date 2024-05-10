9a669bcfebbf29790bdefc6318c0110704462317
Simplyvoda <efem.vodina@gmail.com>
2024-05-10 20:32:31 +0100

    fixed redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,39 +1,39 @@
-import { createClient } from 'redis';
-
+// Class contains the redis client
+import { createClient } from "redis";
 
 class RedisClient {
-	constructor() {
-		this.client = createClient()
-		this.client.on('error', err => console.log('Redis Client Error', err)).
-			connect();
-	}
+  constructor() {
+    this.client = createClient();
 
-	function isAlive() {
-		client.on('error', (err) => {
-  return false
-});
+    // log to the console on error
+    this.client.on("error", (err) => console.log("Redis Client Error", err));
+  }
 
-client.on('connect', () => {
-  return true
-});
+  async isAlive() {
+    // return a promise resolving to either true or false based on the connection
+    return new Promise((resolve, reject) => {
+      this.client.on("connect", () => resolve(true));
+      this.client.on("error", () => resolve(false));
+    });
+  }
 
+  async get(key) {
+    // get the value from the redis database
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async set(key, value, dur) {
+    // set the value in the redis database for the specified duration
+    await this.client.setex(key, dur, value);
+  }
+
+  async del(key) {
+    // delete the value from the redis database
+    await this.client.del(key);
+  }
 }
 
-async function get(key){
-	const value = await this.client.get(key)
-	return value;
-}
+const redisClient = new RedisClient();
 
-async function set(key, value, dur){
-	await this.client.setex(key, dur, value)
-}
-
-async function del(key){
-	await this.client.del(key)
-}
-
-}
-
-const redisClient = RedisClient()
-
-export redisClient;
+module.exports = redisClient;
