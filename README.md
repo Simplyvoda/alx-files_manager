59e6c379bce76c80a02c7319748270133b401350
Simplyvoda <efem.vodina@gmail.com>
2024-05-12 15:53:05 +0100

    made changes to file check

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# alx-files_manager
-Summary of backend-trimester:authentication, NodeJS, MongoDB, Redis, pagination and background processing.
+# File Manager Project 📁🔐
+This project is a culmination of various backend technologies including authentication, NodeJS, MongoDB, Redis, pagination, and background processing. The goal is to create a simple platform for uploading and viewing files, incorporating key features such as user authentication, file listing, upload capabilities, permission management, file viewing, and thumbnail generation for images.
+
+## Learning Objectives 📚
+By the end of this project, you should be able to:
+
+- Explain how to create an API with Express
+- Understand the process of user authentication
+- Describe methods for storing data in MongoDB
+- Implement temporary data storage using Redis
+- Setup and utilize a background worker for asynchronous tasks
+
+
+
--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -214,7 +214,7 @@ export default class FilesController {
                         }
                     }
 
-                    const updatedFileDoc = await (await dbClient.filesCollection()).updateOne({ _id: file._id}, update);
+                    const updatedFileDoc = await (await dbClient.filesCollection()).updateOne({ _id: file._id }, update);
 
                     if (updatedFileDoc.matchedCount === 1) {
                         res.status(200).json(file);
@@ -259,7 +259,7 @@ export default class FilesController {
                         }
                     }
 
-                    const updatedFileDoc = await (await dbClient.filesCollection()).updateOne({ _id: file._id}, update);
+                    const updatedFileDoc = await (await dbClient.filesCollection()).updateOne({ _id: file._id }, update);
 
                     if (updatedFileDoc.matchedCount === 1) {
                         res.status(200).json(file);
@@ -285,7 +285,7 @@ export default class FilesController {
             _id: ObjectId(user_id)
         });
 
-        if(user){
+        if (user) {
             const file_id = req.params.id;
             const file = await (await dbClient.filesCollection()).findOne({
                 _id: ObjectId(file_id),
@@ -297,29 +297,31 @@ export default class FilesController {
             if (!file || !file.isPublic) {
                 res.status(404).json({ error: "Not found" });
             } else {
-                if (file.type === "folder"){
+                if (file.type === "folder") {
                     res.status(400).json({ error: "A folder doesn\'t have content " });
-                }else{
+                } else {
                     // if checker fails this try adding file size to the file path
                     let filePath = file.localPath;
                     if (size) {
-                      filePath = `${file.localPath}_${size}`;
+                        filePath = `${file.localPath}_${size}`;
                     }
-                    const fileData = await readFileAsync(filePath);
-                    if(!fileData){
-                        // if there is no file data or the file is empty
-                        res.status(404).json({ error: "Not found" });
-                    }else{
-                        // by using mime-types get MIME-type based on file name
-                        // return file content with correct mime type
-                        const absoluteFilePath = await realpathAsync(filePath);
-                        const mimeType = mime.lookup(file.name);
-                        res.setHeader('Content-Type', mimeType || 'text/plain; charset=utf-8');
-                        res.status(200).sendFile(absoluteFilePath);
+                    if (existsSync(filePath)) {
+                        const fileInfo = await statAsync(filePath);
+                        if (!fileInfo.isFile()) {
+                            res.status(404).json({ error: 'Not found' });
+                            return;
+                        }
+                    } else {
+                        res.status(404).json({ error: 'Not found' });
+                        return;
                     }
+                    const absoluteFilePath = await realpathAsync(filePath);
+                    const mimeType = mime.lookup(file.name);
+                    res.setHeader('Content-Type', mimeType || 'text/plain; charset=utf-8');
+                    res.status(200).sendFile(absoluteFilePath);
                 }
             }
-        }else{
+        } else {
             res.status(401).json({ error: "Not found" })
         }
 
